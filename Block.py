d5664f11a07bbe9d0b76887be06f479d69bf003b
if164ever1 <zadorozhniak.ihor@gmail.com>
2025-12-13 21:38:09 -0600

    update class Block

--- Block.py
+++ Block.py
@@ -1,10 +1,12 @@
-
+import hashlib
+import json
 
 
 class Block:
     id = None
-    previous_block = None
     history = None
+    parent_id = None
+    parent_hash = None
 
 block_A = Block()
 block_A.id = "A"
@@ -12,10 +14,20 @@ block_A.history = "Nelson likes cats."
 
 block_B = Block()
 block_B.id = "B"
-block_B.previous_block = block_A
 block_B.history = "Maria likes dogs."
+block_B.parent_id = block_A.id
+block_B.parent_hash = hashlib.sha256(
+    json.dumps(block_A.__dict__, sort_keys=True).encode('utf-8')).hexdigest()
+print(block_B.parent_hash)
 
 block_C = Block()
 block_C.id = "C"
-block_C.previous_block = block_B
 block_C.history = "Sky likes birds."
+block_C.parent_id = block_B.id
+block_C.parent_hash = hashlib.sha256(
+    json.dumps(block_B.__dict__, sort_keys=True).encode('utf-8')).hexdigest()
+print(block_C.parent_hash)
+
+print(block_A.__dict__)
+print(block_B.__dict__)
+print(block_C.__dict__)
