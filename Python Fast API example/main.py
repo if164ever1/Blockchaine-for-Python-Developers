fbd1b47f37fdf9c27ed95f2873f07cb37ef7e2c5
if164ever1 <zadorozhniak.ihor@gmail.com>
2025-12-14 12:22:39 -0600

    api sim-ple

---
+++ Python Fast API example/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+counter_file = "counter.txt"
+
+counter = 0
+
+
+@app.get("/")
+async def root():
+    global counter
+    if os.path.isfile(counter_file):
+        with open(counter_file, "r") as f:
+            try:
+                counter = int(f.read().strip())
+            except ValueError:
+                counter = 0
+    counter += 1
+    with open(counter_file, "w") as f:
+        f.write(str(counter))
+    return {"message": f"Hello, World! This endpoint has been accessed {counter} times."}
